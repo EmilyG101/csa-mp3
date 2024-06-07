8a1ea9b4ca8626c4fbb439591020b60dae6cf322
EmilyG101 <101006556+EmilyG101@users.noreply.github.com>
2024-06-07 07:23:46 -0400

    Update wordChecker.java

--- FRQ-Solutions/Q3/wordChecker.java
+++ FRQ-Solutions/Q3/wordChecker.java
@@ -7,7 +7,14 @@ public class wordChecker{
        wordList = words;
      }
 
-     public boolean isWordChain(){
+    /**
+    * Returns true if each element of wordList (except the first)
+    * element as a substring and returns false otherwise, as describe
+    * Precondition: wordList contains at least two elements.
+    * Postcondition: wordList is unchanged.
+    * /
+    /**
+public boolean isWordChain(){
         for(int i =0; i< wordList.size(); i++){
           if(wordList.get(i).indexOf(wordList.get(i+1)) == -1){
             return false;
